6ce1014fcf2b6853779ef12ec288b2056e256132
manav-01 <manavparmar696@gmail.com>
2024-08-02 17:28:15 +0530

    ok

--- app/page.js
+++ app/page.js
@@ -1,17 +1,39 @@
-import SignInForm from "@/pages/SignInForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-
+import SignInForm from "@/pages/SignInForm";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
-
+  // Get session
   const session = await getServerSession(authOptions);
 
-  if(session) redirect("/dashboard");
-  return(
+  // If there's a session, redirect to the dashboard
+  if (session) {
+    redirect("/dashboard");
+    return null; // Return null to avoid rendering the rest of the component
+  }
+
+  return (
     <main>
-      <SignInForm/>
+      <SignInForm />
     </main>
   );
 }
+  
+// import SignInForm from "@/pages/SignInForm";
+// import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
+
+// import { authOptions } from "./api/auth/[...nextauth]/route";
+
+// export default async function Home() {
+
+//   const session = await getServerSession(authOptions);
+
+//   if(session) redirect("/dashboard");
+//   return(
+//     <main>
+//       <SignInForm/>
+//     </main>
+//   );
+// }
