dc13d0d4aae81a5c9cd15ceca24e40d99147d351
manav-01 <manavparmar696@gmail.com>
2024-08-02 19:17:58 +0530

    update making

--- components/Select.jsx
+++ components/Select.jsx
@@ -1,12 +1,30 @@
 import React, { forwardRef } from 'react'
 
 function Select({name, classname, id, options, ...props},ref) {
+   function capitalizeFirstWord(name) {
+     const firstWord = name.split(" ")[0];
+     return (
+       firstWord.charAt(0).toUpperCase() + firstWord.slice(1).toLowerCase()
+     );
+   }
+
   return (
-   <select name={name} id={id} className={`bg-transparent ${classname}`} {...props} ref={ref}>
-            <option value="">Not Selected</option>
-            {options && options.map(option => <option value={option} key={option}>{option}</option> )}
+    <select
+      name={name}
+      id={id}
+      className={`bg-transparent ${classname}`}
+      {...props}
+      ref={ref}
+    >
+      <option value="">Not Selected</option>
+      {options &&
+        options.map((option) => (
+          <option value={option} key={option}>
+            {capitalizeFirstWord(option)}
+          </option>
+        ))}
     </select>
-  )
+  );
 }
 
 export default forwardRef(Select);
