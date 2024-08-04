3a927f72d1a4544e52d443ab4fe25347f7f2289d
manav-01 <manavparmar696@gmail.com>
2024-08-04 12:59:08 +0530

    remove script

--- app/layout.js
+++ app/layout.js
@@ -24,10 +24,6 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:wght@100..900&display=swap"
           rel="stylesheet"
         />
-        <script
-          src="https://cdn.tiny.cloud/1/no-api-key/tinymce/7/tinymce.min.js"
-          referrerpolicy="origin"
-        ></script>
       </head>
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
